752424d87af3f4ccd9d31f8752d2f804e0985fae
radja <radjasatrio70@gmail.com>
2025-10-31 09:03:25 +0700

    create page splash screen scaffold awal

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'SplashScreen1.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Nim: 1123150172, Radja",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+        ), // Sesuaikan seedColor
+      ),
+      home: const SplashScreen1(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
